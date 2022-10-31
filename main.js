e4586ec6a33fabdd143210f9a5be02e6cf40a26f
Rashedul Islam <rashedul.rajon@gmail.com>
2022-10-31 20:02:26 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+var billGates = {shirt:true, shirtColor:"offWhite", smile:true, swatter:true, swatterColor:"Gray", glass:true}
+
+
+//console.log(billGates['shirt']);
+
+var billGatesPro={
+    shirt:{
+        color:"offWhite",
+        quality:"Good",
+        price:"200USD"
+    },
+    swatter:{
+        color:"Gray",
+        quality:"Good",
+        price:"230USD",
+        warm:"best"
+    },
+    face:{
+        smiling:"yes",
+        chosma:"yes",
+        teeth:"White"
+    }
+}
+
+console.log(billGatesPro['swatter']['price']);
